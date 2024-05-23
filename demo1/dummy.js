1cc6bfe91ccb47d033044e7b3d337c8b64d1f3cd
Ashutosh.Gupta <ashutosh.gupta@unthinkable.co>
2024-05-23 15:26:58 +0530

    created a demo project

---
+++ demo1/dummy.js
@@ -0,0 +1,21 @@
+// const http = require('node:http');
+// const express = require('express');
+// const app = express();
+const os = require('os');
+
+console.log(os.cpus());
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// app.use
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// app.listen(port, hostname, () => {
+//   console.log(`🚀🚀Server running at http://${hostname}:${port}/`);
+// });
